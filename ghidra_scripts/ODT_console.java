d29e71491ef7dff82782a824db0a61eae8351e5b
kaunomedis <87241527+kaunomedis@users.noreply.github.com>
2026-07-29 20:20:11 +0300

    Create ODT_console.java

---
+++ ghidra_scripts/ODT_console.java
@@ -0,0 +1,31 @@
+//PDP11 Octal Debugging Technique (ODT) console interface
+//@author 
+//@category PDP11 Hardware
+//@keybinding 
+//@menupath 
+//@toolbar 
+//@runtime Java
+
+import ghidra.app.script.GhidraScript;
+import ghidra.program.model.util.*;
+import ghidra.program.model.reloc.*;
+import ghidra.program.model.data.*;
+import ghidra.program.model.block.*;
+import ghidra.program.model.symbol.*;
+import ghidra.program.model.lang.protorules.*;
+import ghidra.program.model.scalar.*;
+import ghidra.program.model.mem.*;
+import ghidra.program.model.listing.*;
+import ghidra.program.model.lang.*;
+import ghidra.program.model.pcode.*;
+import ghidra.program.model.address.*;
+import ghidra.program.model.data.ISF.*;
+
+public class ODT_console extends GhidraScript {
+
+    public void run() throws Exception {
+//TODO Add User Code Here
+    }
+
+}
+
